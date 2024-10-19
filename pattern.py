f48ce3c75301d46057263ec60531edd5ef43a5a5
ShlokThandole <tgrao940@gmail.com>
2024-10-19 22:09:24 +0530

    add loop

---
+++ pattern.py
@@ -0,0 +1,7 @@
+n = int(input("enter number of rows"))
+
+for i in range (0,n):
+    for j in range (0,i+11):
+        print("*", end="")
+
+    print("\n")
---
+++ while.py
@@ -0,0 +1,7 @@
+num = 1
+sum = 0
+
+while(num<=10):
+    sum = sum+num
+    num = num+1
+print("the sum of first 10 natural number = ", sum)
